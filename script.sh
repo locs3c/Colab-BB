4bb0e8b333ff2bae9e7b021a055655ededd48e90
Lucas Oliveira <lucasinfobarretos@gmail.com>
2021-08-07 10:48:45 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
+# Instalando GO Lang
 echo "Installing Golang"
-wget https://golang.org/dl/go1.15.linux-amd64.tar.gz
+wget https://golang.org/dl/go1.16.7.linux-amd64.tar.gz
 sudo tar -xvf go1.15.linux-amd64.tar.gz
 sudo mv go /usr/local
 rm -rf go1.15.linux-amd64.tar.gz
@@ -13,7 +15,21 @@ echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bash_profile
 cp /usr/local/go/bin/go /bin/
 mkdir /root/loc-recon/
 cd /root/loc-recon/
+# Instalndo Subfinder
 git clone https://github.com/projectdiscovery/subfinder.git
 cd /root/loc-recon/subfinder/v2/cmd/subfinder
 go build .
 mv subfinder /bin/
+cd /root/loc-recon/
+#Instalando Chaos
+git clone https://github.com/projectdiscovery/chaos-client.git
+cd chaos-client/cmd/chaos/
+go build
+cp chaos /bin/
+cd /root/loc-recon/
+#Instalando AssetFinder
+git clone https://github.com/tomnomnom/assetfinder.git
+cd assetfinder
+go build
+cp assetfinder /bin/
+cd /root/loc-recon/
